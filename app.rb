4b4f2926a6a891ce559afe6a49ab9d6a4057f1c2
olmesm <olmesm@gmail.com>
2016-03-14 17:18:50 +0000

    named cat

--- app.rb
+++ app.rb
@@ -8,7 +8,12 @@ get '/secret' do
   "Spill your guts!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  @name = params[:name]
   erb(:index)
 end
